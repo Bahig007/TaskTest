e45c602615e3f12a01e664da04842017bb79febd
bahig007 <ahmedbahig.mahmoud@gmail.com>
2025-01-30 03:07:46 -0800

    secondCommit

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -42,7 +43,9 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
+            if ($e instanceof ValidationException) {
+                return response()->json($e->errors(), 422);
+            }
         });
     }
 }
